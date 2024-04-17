fd9e6cb979778a86bef3a920071a65b0ea217d8e
Muhammad Anas Jamshaid <145829950+muhammadanasjamshaid@users.noreply.github.com>
2024-04-17 13:50:47 +0500

    first commit

---
+++ index.ts
@@ -0,0 +1,24 @@
+#! usr/bin/env node
+
+
+
+// Import the "Inqurir" Module, which is A COMMAND LINE  interface for Node.js
+import inquirer from 'inquirer'
+
+// Declare a contant "answer" and assign it to the result of inquirer.promt fuction
+const answers : {
+    Sentence : string
+} = await inquirer.prompt([{
+    name: "Sentence",
+    type : "input",
+    message : "Enter Your Sentence to count the word: "
+}])
+
+const word = answers.Sentence.trim().split(" ")
+
+// print the arry of word to the console
+console.log(word);
+
+// print the word count of the sentence to the console
+
+console.log(`Your Sentence word count is ${word.length}`);
